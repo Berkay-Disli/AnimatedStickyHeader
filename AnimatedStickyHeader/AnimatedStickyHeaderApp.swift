d3c9cab20d5594bde194e1c27bbb05d46d299c24
Berkay-Disli <berkaydisli.work@gmail.com>
2023-01-20 12:52:06 +0300

    Initial Commit

---
+++ AnimatedStickyHeader/AnimatedStickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyHeaderApp.swift
+//  AnimatedStickyHeader
+//
+//  Created by Berkay Disli on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
